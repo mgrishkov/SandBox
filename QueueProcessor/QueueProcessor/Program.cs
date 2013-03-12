ee8f022f44c02610d3f922dd3d860519faff3c64
Maxim Grishkov <grishkov.mn@gmail.com>
2013-03-12 23:20:18 +0400

    * v.1

---
+++ QueueProcessor/QueueProcessor/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace QueueProcessor
+{
+    class Program
+    {
+        private class Request : IRequest
+        {
+            private static Random random = new Random();
+
+            public int ID { get; set; }
+            public void Execute()
+            {
+                
+                int randomNumber = random.Next(1000, 10000);
+
+                Thread.Sleep(randomNumber);
+                Console.WriteLine("-- Request #{0} was processed by thread #{1} within task #{2}", ID, Thread.CurrentThread.ManagedThreadId, Task.CurrentId);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            using (var requests = new QueueProcessor<Request>(5))
+            {
+                requests.RequesExecuting += requests_RequesExecuting;
+                requests.QueueProcessed += requests_QueueProcessed;
+                requests.Add(new Request() { ID = 101 });
+                requests.Add(new Request() { ID = 102 });
+                requests.Add(new Request() { ID = 103 });
+                requests.Add(new Request() { ID = 104 });
+                requests.Add(new Request() { ID = 105 });
+                requests.Add(new Request() { ID = 106 });
+                requests.Add(new Request() { ID = 107 });
+                requests.Add(new Request() { ID = 108 });
+                requests.Add(new Request() { ID = 109 });
+                requests.Add(new Request() { ID = 110 });
+                requests.Add(new Request() { ID = 111 });
+                requests.Add(new Request() { ID = 112 });
+                requests.Add(new Request() { ID = 113 });
+                requests.Add(new Request() { ID = 114 });
+                requests.Add(new Request() { ID = 115 });
+                requests.Add(new Request() { ID = 116 });
+                requests.Add(new Request() { ID = 117 });
+                requests.Add(new Request() { ID = 118 });
+                requests.Add(new Request() { ID = 119 });
+                requests.Add(new Request() { ID = 120 });
+                requests.Add(new Request() { ID = 121 });
+                requests.Add(new Request() { ID = 122 });
+                requests.Add(new Request() { ID = 123 });
+                requests.Add(new Request() { ID = 124 });
+                requests.Add(new Request() { ID = 125 });
+                requests.Add(new Request() { ID = 126 });
+                requests.Add(new Request() { ID = 127 });
+                requests.Add(new Request() { ID = 128 });
+                requests.Add(new Request() { ID = 129 });
+                requests.Add(new Request() { ID = 130 });
+                requests.Add(new Request() { ID = 131 });
+                requests.Add(new Request() { ID = 132 });
+
+                requests.Process();
+            };
+            Console.ReadKey();
+        }
+
+        static void requests_QueueProcessed(object sender, EventArgs e)
+        {
+            Console.WriteLine("Queue was processed");
+        }
+
+        static void requests_RequesExecuting(object sender, QueueProcessor<Program.Request>.RequesExecutingEventArgs<Program.Request> e)
+        {
+            Console.WriteLine("Request #{0} change state to {1}", e.Request.ID, e.State);
+        }
+
+        
+    }
+}
---
+++ QueueProcessor/QueueProcessor/QueueProcessor.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace QueueProcessor
+{
+    public interface IRequest
+    {
+        void Execute();
+    }
+    
+    public class QueueProcessor<T> : IDisposable where T : IRequest
+    {
+        public class RequesExecutingEventArgs<T> : EventArgs
+        {
+            public enum EStatus
+            {
+                Executing = 1,
+                Comleted = 2,
+                Failed = 3
+            }
+
+            public T Request { get; set; }
+            public EStatus State { get; set; }
+            public string Message { get; set; }
+        }
+
+        public event EventHandler<RequesExecutingEventArgs<T>> RequesExecuting;
+        public event EventHandler QueueProcessed;
+
+        private BlockingCollection<T> _queue;
+        private readonly int _maxConcurrent;
+        private List<Task> _tasks;
+        private volatile bool _locker;
+
+        public QueueProcessor(int MaxConcurrent)
+        {
+            _maxConcurrent = MaxConcurrent;
+            _queue = new BlockingCollection<T>();
+            _tasks = new List<Task>();
+        }
+        public void Process()
+        {
+            _queue.CompleteAdding();
+            for (int i = 0; i < _maxConcurrent; i++)
+            {
+                if (!_queue.IsCompleted)
+                {
+                    var task = Task.Factory.StartNew(() => processRequest());
+                    _tasks.Add(task);
+                };
+            };
+            Task.WaitAll(_tasks.ToArray());
+            OnQueueProcessed();
+        }
+        private void processRequest()
+        {
+            if (!_queue.IsCompleted)
+            {
+                T request = _queue.Take();
+                Execute(request);
+                if (!_queue.IsCompleted)
+                {
+                    var task = Task.Factory.StartNew(() => processRequest());
+                    task.Wait();
+                };
+            };
+        }
+        private void Execute(T request)
+        {
+            var eventArgs = new RequesExecutingEventArgs<T>()
+            {
+                Request = request,
+                State = RequesExecutingEventArgs<T>.EStatus.Executing,
+                Message = String.Empty
+            };
+            OnRequesExecuting(eventArgs);
+            try
+            {
+                request.Execute();
+                eventArgs.State = RequesExecutingEventArgs<T>.EStatus.Comleted;
+            }
+            catch (Exception e)
+            {
+                eventArgs.State = RequesExecutingEventArgs<T>.EStatus.Failed;
+                eventArgs.Message = e.Message;
+            };
+        }
+        protected virtual void OnRequesExecuting(RequesExecutingEventArgs<T> e)
+        {
+            if (RequesExecuting != null)
+            {
+                RequesExecuting(this, e);
+            };
+        }
+        protected virtual void OnQueueProcessed()
+        {
+            if (QueueProcessed != null)
+            {
+                QueueProcessed(this, new EventArgs());
+            };
+        }
+        
+        public void Add(T Request)
+        {
+            _queue.Add(Request);
+        }
+
+#region Implement IDispose
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_queue != null)
+                {
+                    _queue.Dispose();
+                    _queue = null;
+                };
+                if (_tasks != null)
+                {
+                    _tasks.ForEach(x => x.Dispose());
+                    _tasks.Clear();
+                };
+            };
+        }
+        ~QueueProcessor()
+        {
+            Dispose(false);
+        }
+#endregion
+    }
+}
