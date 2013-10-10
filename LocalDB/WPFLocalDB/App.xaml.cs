98131384376ce7849c0a09d63fede7a510fc8a87
Maxim Grishkov <grishkov.mn@gmail.com>
2013-10-10 19:43:28 +0400

    *

---
+++ LocalDB/WPFLocalDB/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WPFLocalDB.Models;
+
+namespace WPFLocalDB
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            using (var dc = new DocumentDataContext())
+            {
+                if (!dc.DatabaseExists())
+                {
+                    dc.CreateDatabase();
+                }
+            }
+        }
+    }
+}
---
+++ LocalDB/WPFLocalDB/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFLocalDB.Models;
+
+namespace WPFLocalDB
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        DocumentDataContext _dc;
+
+        public List<Document> Documents
+        {
+            get
+            {
+                return _dc.Documents.ToList();
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _dc = new DocumentDataContext();
+            this.DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var doc = new Document()
+            {
+                Title = "Test",
+                Author = "TestUser",
+                Body = "zxczczczcz",
+                CreationTime = DateTime.Now
+            };
+            _dc.Documents.InsertOnSubmit(doc);
+            _dc.SubmitChanges();
+            RaisePropertyChanged("Documents");
+
+        }
+    }
+}
