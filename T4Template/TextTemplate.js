c920895574f5d542c8bfa67b3c38447ad47282a8
Maxim grishkov <grishkov.mn@gmail.com>
2014-01-26 13:19:15 +0400

    * T4TemplateSample

---
+++ T4Template/TextTemplate.js
@@ -0,0 +1,11 @@
+﻿
+
+var Resources = new Array();
+Resources["EN"] = {
+	TooManyRows : 'Error message 2',
+	FileNotFound : 'Error message 1',
+};
+Resources["RU"] = {
+	TooManyRows : 'Ошибка 2',
+	FileNotFound : 'Ошибка 1',
+};
