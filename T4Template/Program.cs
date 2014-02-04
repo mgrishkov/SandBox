1453fd6d9e1452303853026522c4c51a3bbc1ecc
Maxim grishkov <grishkov.mn@gmail.com>
2014-02-04 22:03:22 +0400

    * <-

--- T4Template/Program.cs
+++ T4Template/Program.cs
@@ -5,26 +5,6 @@ namespace T4Template
     {
         static void Main(string[] args)
         {
-            var sb = new System.Text.StringBuilder();
-            
-            string[] cultures = { "en-GB", "ru-RU" };
-
-            foreach (var culture in cultures)
-            {
-                var ci = System.Globalization.CultureInfo.GetCultureInfo(culture);
-                var resource = System.String.Format("Resources.{0}", ci.TwoLetterISOLanguageName.ToUpper());
-                sb.AppendLine(System.String.Format("{0} = {{}};", resource));
-
-                var resourceSet = Resources.Errors.ResourceManager.GetResourceSet(ci, true, true);
-                foreach (System.Collections.DictionaryEntry entry in resourceSet)
-                {
-                    sb.AppendLine(System.String.Format("{0}.{1} = \'{2}\';", resource, entry.Key, entry.Value));
-                }
-            };
-
-            Console.Write(sb.ToString());
-            
-            Console.ReadKey();
         }
     }
 }
