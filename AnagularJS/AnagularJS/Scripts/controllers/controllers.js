8888ece8ac8d6f64e00566238c73bb76b96567e7
Maxim grishkov <grishkov.mn@gmail.com>
2015-04-06 17:59:49 +0300

    *

---
+++ AnagularJS/AnagularJS/Scripts/controllers/controllers.js
@@ -0,0 +1,12 @@
+﻿var newControllers = angular.module('newControllers', []);
+
+newControllers.controller('HomeCtrl', function ($scope, $http) {
+    $http.post('Home/GetDriversList', null).success(function (data) {
+        $scope.drivers = data;
+    });
+
+    $scope.searchBy = {};
+
+    $scope.searchByID = function (actual, expected) { return angular.equals(expected, actual) };
+});
+
