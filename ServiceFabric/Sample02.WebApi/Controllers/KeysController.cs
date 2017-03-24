accf1fa8cc5fedd7127370040ecc44ee62357ff5
grishkov.mn <grishkov.mn@gmail.com>
2017-03-25 02:28:38 +0300

    servicefabric sample

---
+++ ServiceFabric/Sample02.WebApi/Controllers/KeysController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sample02.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class KeysController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "key1", "key2" };
+        }
+    }
+}
